a7ac2f1fcf35b0e986e5bae6e537f5513ded7361
Oscar_Rivera <oscarriveramayor@hotmail.com>
2025-09-22 22:14:52 -0500

    Init project

---
+++ src/test/java/com/project/cafeteria/cafeteriaapp/CafeteriaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cafeteria.cafeteriaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeteriaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
